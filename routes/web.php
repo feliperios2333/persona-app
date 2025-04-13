cbb533cfa889808583d1da686bf814fb1ebad752
Felipe Rios <feliperios2003brs@gmail.com>
2025-04-13 10:43:58 -0500

    Municipality list

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Comunacontroller; // Importa el controlador Comunacontroller
+use App\Http\Controllers\MunicipioController; // Importa el controlador MunicipioController
 use App\Models\Comuna;
 
 /*
@@ -18,11 +19,14 @@ use App\Models\Comuna;
 Route::get('/', function () {
     return view('welcome');
 });
-
+//rutas para el controlador de comuna
 Route::get('/comuna', [Comunacontroller::class, 'index']) ->name ('comuna.index');
 Route ::post('/comuna',[Comunacontroller :: class, 'store']) -> name ('comuna.store'); 
 Route ::get ('/comuna/create',[Comunacontroller :: class, 'create']) -> name ('comuna.create');
 Route :: delete ('/comuna/{comuna}',[Comunacontroller :: class, 'destroy']) -> name ('comuna.destroy');
 Route :: put('/comuna/{comuna}',[Comunacontroller :: class, 'update']) -> name ('comuna.update');
 Route :: get ('/comuna/{comuna}/edit',[Comunacontroller :: class, 'edit']) -> name ('comuna.edit');
-// Ruta para mostrar el listado de comunas
+
+
+//Rutas para el controlador de municipio
+Route::get('/municipio', [MunicipioController::class, 'index']);
